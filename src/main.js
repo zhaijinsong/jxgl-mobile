3f1cc36dd529eea883b98932e221bce8da7e8286
Tjundong <tjundong853@163.com>
2020-12-14 09:08:10 +0800

    增加mint-ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'lib-flexible'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 
 Vue.config.productionTip = false
+Vue.use(MintUI)
 
 new Vue({
     render: h => h(App)
