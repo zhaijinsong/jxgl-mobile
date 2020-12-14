585c40a0969c6d387b576591acfe6399f1af664c
zhaijinsong <1633825587@qq.com>
2020-12-14 08:57:41 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class='heade'>机械管理系统</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  
+}
+.heade {
+    height: 40px;
+    font-size: 16px;
+    background: skyblue;
+    text-align: center;
+    line-height: 40px;
+  }
+</style>
